1e7ddddff4c3b6448aff1cfc9a5deec5bf06a135
minkolee <minkolee@gmail.com>
2019-02-02 09:58:54 +0800

    连接池写不出来,抓狂

--- src/EnhancedMySource/MySource/MyThread.java
+++ src/EnhancedMySource/MySource/MyThread.java
@@ -19,13 +19,13 @@ public class MyThread implements Runnable {
             PreparedStatement pst = connection.prepareStatement("SELECT * FROM goods");
             ResultSet rs = pst.executeQuery();
             while (rs.next()) {
-                System.out.println(rs.getInt("id") + " | " + rs.getString("name") + " | " + rs.getInt("quantity"));
+                System.out.println(rs.getInt("id") + " || " + rs.getString("name") + " || " + rs.getInt("quantity"));
             }
 
             //这里必须释放回去,否则后边线程在阻塞,无法获得连接对象
             connection.close();
             //但是这里有个问题是,还调用了自己写的release方法,如何能增强.close()方法呢
-            System.out.println("-------------------------------关闭成功--------------------------------");
+            System.out.println("------" + Thread.currentThread().getName() + "----关闭成功--------------------------------");
         } catch (SQLException ex) {
             ex.printStackTrace();
             throw new RuntimeException(ex);
